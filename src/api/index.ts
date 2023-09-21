422f41b0976a52d39820d6d012235577ee8385da
曹绪培 <2877406366@qq.com>
2023-09-22 00:59:36 +0800

    整体样式优化

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ import useUserStore from "@/store/user";
 
 // 创建 Axios 实例
 const instance = axios.create({
-    baseURL: 'http://124.70.45.144:8081', // 替换成你的后端 API 地址
+    baseURL: 'https://pan.reaicc.com', // 替换成你的后端 API 地址
 });
 
 // 发起 HTTP 请求的函数
