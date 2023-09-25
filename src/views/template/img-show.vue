59a10013529ae6852eb280ae64def6bde17ab4d1
曹绪培 <2877406366@qq.com>
2023-09-25 22:43:20 +0800

    排行样式优化

--- src/views/template/img-show.vue
+++ src/views/template/img-show.vue
@@ -1363,7 +1363,7 @@ button.vjs-play-control.vjs-control.vjs-button {
     font-size: 14px;
   }
   .main-blog__author {
-    flex-direction: column-reverse;
+    //flex-direction: column-reverse;
     align-items: flex-start;
   }
   .author-detail {
